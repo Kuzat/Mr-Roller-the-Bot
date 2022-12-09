162e2ac7b9995e97d0bc48559ecc211f78bf8128
Vegard Bjørsvik <vegardbjr@gmail.com>
2022-12-09 16:34:34 +0100

    total rolls -> total rolled

--- roller_bot/models/user.py
+++ roller_bot/models/user.py
@@ -13,4 +13,4 @@ class UserTotalRolls(BaseModel):
     total_rolls: int
 
     def __str__(self) -> str:
-        return f'{self.user.mention if self.user.mention else self.user.id}: {self.total_rolls} total rolls'
+        return f'{self.user.mention if self.user.mention else self.user.id}: {self.total_rolls} total rolled'
