832380703df67e01d95859cddcb8b6db5ce66b5f
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-02-16 09:56:02 +0100

    save to file first

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 set -e -x
 
-# SSH into the server using the key in environment variable SSH_KEY
-eval "$(ssh-agent -s)"
-ssh-add - <<< "$SSH_KEY"
-ssh "$REMOTE_USER"@"$REMOTE_HOST"
 cd Mr-Roller-the-Bot
 kill "$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
 poetry run backup
