22670f66f47fefd946133ceb11667d48232b0202
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-02-16 09:27:37 +0100

    Adding the deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+set -e
+
+# SSH into the server using the key in environment variable SSH_KEY
+ssh -i <(echo "$SSH_KEY") -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_HOST"
+cd Mr-Roller-the-Bot
+kill "$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
+poetry run backup
+git pull
+poetry install
+nohup poetry run bot &
