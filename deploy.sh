70d8609669a96cc294e5efe6fca22af346da015d
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-02-16 09:36:23 +0100

    Adding key to ssh-add

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 set -e
 
 # SSH into the server using the key in environment variable SSH_KEY
-ssh -i <(echo "$SSH_KEY") -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_HOST"
+ssh-add - <<< "$SSH_KEY"
+ssh "$REMOTE_USER"@"$REMOTE_HOST"
 cd Mr-Roller-the-Bot
 kill "$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
 poetry run backup
