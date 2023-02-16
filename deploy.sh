583eb2ae0e764757e4839007ef5d0fdf6608ed69
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-02-16 10:31:20 +0100

    Make sure we exit after update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 set -e -x
 
-ls
 cd Mr-Roller-the-Bot
-ls
 kill "$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
 poetry run backup
 git pull
 poetry install
 nohup poetry run bot &
+# Exit with success code
+exit 0
