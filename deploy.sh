dcee373732d3d0de6fae446ea08cd787a38e9ba3
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-04-25 19:00:22 +0200

    Try to source bashrc first

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e -x
+# Source bashrc
+. ~/.bashrc
 
 cd Mr-Roller-the-Bot
 BOT_PID="$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
 if [ -n "$BOT_PID" ]; then
   kill "$BOT_PID"
-  .local/bin/poetry run backup
+  poetry run backup
 fi
 
 git pull
-.local/bin/poetry install
-nohup .local/bin/poetry run bot > nohup.out 2> nohup.err < /dev/null &
+poetry install
+nohup poetry run bot > nohup.out 2> nohup.err < /dev/null &
