d56a114b6d82854672363d5741bf6699f16bd3aa
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-04-25 18:51:35 +0200

    Add a check if we found a process before trying to delete

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 set -e -x
 
 cd Mr-Roller-the-Bot
-kill "$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
-poetry run backup
+BOT_PID="$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
+if [ -n "$BOT_PID" ]; then
+  kill "$BOT_PID"
+  poetry run backup
+fi
+
 git pull
 poetry install
 nohup poetry run bot > nohup.out 2> nohup.err < /dev/null &
