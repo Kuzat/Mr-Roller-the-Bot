6b4afeb76d734ab5c6359de6da5132d091c8258a
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-04-25 18:57:24 +0200

    Use local poetry version

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ cd Mr-Roller-the-Bot
 BOT_PID="$(ps -ef | grep '[r]oller_bot.main' | awk '{print $2}')"
 if [ -n "$BOT_PID" ]; then
   kill "$BOT_PID"
-  poetry run backup
+  .local/bin/poetry run backup
 fi
 
 git pull
-poetry install
-nohup poetry run bot > nohup.out 2> nohup.err < /dev/null &
+.local/bin/poetry install
+nohup .local/bin/poetry run bot > nohup.out 2> nohup.err < /dev/null &
