b32d44921b538da2082b369aa728adbbf6551b43
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-07-19 22:27:10 +0200

    Added option to use other database engine

--- roller_bot/database.py
+++ roller_bot/database.py
@@ -1,21 +1,20 @@
 import os
 from datetime import datetime
+from typing import Optional
 
 import boto3
 import discord
-
-from typing import Optional
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from roller_bot.models.user import User
 from roller_bot.models.base import Base
+from roller_bot.models.user import User
 
 
 class RollDatabase:
     def __init__(self, db_path) -> None:
         self.db_path = db_path
-        self.engine = create_engine(f'sqlite:///{db_path}')
+        self.engine = create_engine(db_path)
 
         # Create the models
         Base.metadata.create_all(self.engine)
@@ -24,7 +23,7 @@ class RollDatabase:
         self.session = self.Session()
 
         # client to s3 for backups
-        self.s3_client = boto3.client('s3')
+        self.s3_client = boto3.client("s3")
 
     def commit(self) -> None:
         self.session.commit()
@@ -48,15 +47,23 @@ class RollDatabase:
     async def backup(self, user: discord.User) -> None:
         # Send the database file to the user as attachment
         current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        await user.send(content=f"[{current_time}]: Backup of database {self.db_path}", file=discord.File(self.db_path))
+        await user.send(
+            content=f"[{current_time}]: Backup of database {self.db_path}",
+            file=discord.File(self.db_path),
+        )
 
     async def backup_to_s3(self, user: discord.User) -> None:
         # Get env from environment variables
-        env = os.getenv('ENV')
+        env = os.getenv("ENV")
         if env is None:
-            raise ValueError('ENV is not set in .env or as environment variable')
+            raise ValueError("ENV is not set in .env or as environment variable")
 
-        s3_path = RollDatabase.backup_s3(self.db_path, self.s3_client, env, db_version=self.db_path.split('_v')[-1].split('.')[0])
+        s3_path = RollDatabase.backup_s3(
+            self.db_path,
+            self.s3_client,
+            env,
+            db_version=self.db_path.split("_v")[-1].split(".")[0],
+        )
 
         await user.send(content=f"Backup of database {s3_path} to S3 successful")
 
@@ -65,12 +72,14 @@ class RollDatabase:
         # Upload the database file to s3
 
         if env is None:
-            raise ValueError('ENV is not set in .env or as environment variable')
+            raise ValueError("ENV is not set in .env or as environment variable")
 
-        s3_path = f'backups/env={env}/version={db_version}/{datetime.now().isoformat()}.db'
+        s3_path = (
+            f"backups/env={env}/version={db_version}/{datetime.now().isoformat()}.db"
+        )
 
         # TODO: bucket name could be added to a parameter store then retrieved from there as environment variable
-        s3_client.upload_file(db_path, 'daily-dice-roller-db-backup-pro', s3_path)
+        s3_client.upload_file(db_path, "daily-dice-roller-db-backup-pro", s3_path)
 
-        print(f'Database successfully backed up to {s3_path}')
+        print(f"Database successfully backed up to {s3_path}")
         return s3_path
--- roller_bot/main.py
+++ roller_bot/main.py
@@ -1,20 +1,18 @@
 import importlib
+import logging
 import os
 from datetime import datetime
 
 import boto3
-import discord
 import click as click
+import discord
+import sentry_sdk
 from dotenv import load_dotenv
 from pydantic import BaseModel
 
 from roller_bot.clients.bots.database_bot import DatabaseBot
-import logging
-
 from roller_bot.database import RollDatabase
 from roller_bot.utils.asyncs import coro
-
-import sentry_sdk
 
 CURRENT_DB_VERSION = 5
 
@@ -28,12 +26,12 @@ class DatabaseBackup(BaseModel):
     @classmethod
     def from_s3_key(cls, key: str):
         # backup/env=environment/version=version/timestamp.db
-        environment, version, timestamp = key.split('/')[1:]
+        environment, version, timestamp = key.split("/")[1:]
         return cls(
-                environment=environment.split('=')[1],
-                version=int(version.split('=')[1]),
-                timestamp=datetime.fromisoformat(timestamp[:-3]),
-                key=key
+            environment=environment.split("=")[1],
+            version=int(version.split("=")[1]),
+            timestamp=datetime.fromisoformat(timestamp[:-3]),
+            key=key,
         )
 
 
@@ -43,81 +41,102 @@ def load_secrets():
 
 
 @click.command()
-@click.option('--db-version', default=CURRENT_DB_VERSION, help='The database version to migrate to.', type=int)
+@click.option(
+    "--db-version",
+    default=CURRENT_DB_VERSION,
+    help="The database version to migrate to.",
+    type=int,
+)
 def download_latest_db(db_version: int):
     # download the latest db from s3 and migrate it if necessary
     # if the db is not found, create a new one
     load_secrets()
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
 
-    print('Downloading latest db from s3...')
+    print("Downloading latest db from s3...")
 
     # Get the environment to look for db
-    env = os.getenv('ENV')
+    env = os.getenv("ENV")
     if env is None:
-        raise ValueError('ENV is not set in .env or as an environment variable.')
+        raise ValueError("ENV is not set in .env or as an environment variable.")
 
     # Get the latest db version
-    db_versions_request = s3_client.list_objects_v2(Bucket='daily-dice-roller-db-backup-pro', Prefix=f'backups/env={env}/version={CURRENT_DB_VERSION}/')
+    db_versions_request = s3_client.list_objects_v2(
+        Bucket="daily-dice-roller-db-backup-pro",
+        Prefix=f"backups/env={env}/version={CURRENT_DB_VERSION}/",
+    )
 
-    if db_versions_request['KeyCount'] == 0:
-        print(f'No db found. for env={env} and version={CURRENT_DB_VERSION}.')
+    if db_versions_request["KeyCount"] == 0:
+        print(f"No db found. for env={env} and version={CURRENT_DB_VERSION}.")
         return
 
-    print(f'Found {db_versions_request["KeyCount"]} db versions. Sorting and getting latest...')
+    print(
+        f'Found {db_versions_request["KeyCount"]} db versions. Sorting and getting latest...'
+    )
     # Sort the keys by the date in the object name
-    db_versions = sorted(db_versions_request['Contents'], key=lambda x: x['Key'], reverse=True)
+    db_versions = sorted(
+        db_versions_request["Contents"], key=lambda x: x["Key"], reverse=True
+    )
 
-    latest_version = DatabaseBackup.from_s3_key(db_versions[0]['Key'])
+    latest_version = DatabaseBackup.from_s3_key(db_versions[0]["Key"])
 
-    print(f'Latest db version is {latest_version.key}. Downloading...')
+    print(f"Latest db version is {latest_version.key}. Downloading...")
     # Download the latest db
-    local_path = f'rolls_v{latest_version.version}.db'
-    s3_client.download_file('daily-dice-roller-db-backup-pro', latest_version.key, local_path)
+    local_path = f"rolls_v{latest_version.version}.db"
+    s3_client.download_file(
+        "daily-dice-roller-db-backup-pro", latest_version.key, local_path
+    )
 
-    print(f'Downloaded latest db. Saved as {local_path}.')
-    print('Check if migration is necessary...')
+    print(f"Downloaded latest db. Saved as {local_path}.")
+    print("Check if migration is necessary...")
 
 
 @click.command()
-@click.option('--db-path', type=str, default=f'rolls_v{CURRENT_DB_VERSION}.db')
+@click.option("--db-path", type=str, default=f"rolls_v{CURRENT_DB_VERSION}.db")
 def backup_db(db_path: str):
     load_secrets()
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
 
-    print('Backing up database...')
+    print("Backing up database...")
 
     # Create object path based on current time and version of db
-    env: str = os.getenv('ENV')
+    env: str = os.getenv("ENV")
     if env is None:
-        raise ValueError('ENV is not set in .env or as environment variable')
+        raise ValueError("ENV is not set in .env or as environment variable")
 
     RollDatabase.backup_s3(db_path, s3_client, env, CURRENT_DB_VERSION)
 
 
 @click.command()
-@click.argument('db_version', type=int)
+@click.argument("db_version", type=int)
 def migrate(db_version: int):
     # Get the migrate command from migration module and run it
-    db_migration = importlib.import_module(f'roller_bot.migrations.db_v{db_version}')
+    db_migration = importlib.import_module(f"roller_bot.migrations.db_v{db_version}")
     db_migration.migrate()
 
 
 # Run the main bot function
 @click.command()
-@click.option('--debug', is_flag=True)
-@click.option('--db-version', type=int, default=CURRENT_DB_VERSION)
+@click.option("--debug", is_flag=True)
+@click.option("--db-version", type=int, default=CURRENT_DB_VERSION)
 @coro
 async def main(debug: bool, db_version: int):
-    print('Debug mode:', debug)
-    # enable sqlalchemy logging in debug mode
-    db_path = f'db/rolls_v{db_version}.db'
-    if debug:
-        logging.basicConfig()
-        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-        db_path = f'rolls_v{db_version}.db'
+    print("Debug mode:", debug)
 
-    print(f"Running with database {db_path}")
+    # Check if DB_URL is set then use that to connect to the database
+    db_url = os.getenv("DB_URL")
+    if db_url:
+        print("DB_URL found. Using that to connect to the database.")
+        db_path = db_url
+    else:
+        # enable sqlalchemy logging in debug mode
+        db_path = f"sqlite:///db/rolls_v{db_version}.db"
+        if debug:
+            logging.basicConfig()
+            logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+            db_path = f"sqlite:///rolls_v{db_version}.db"
+
+        print(f"Running with database {db_path}")
 
     # Load the environment variables from the .env file
     load_secrets()
@@ -125,12 +144,11 @@ async def main(debug: bool, db_version: int):
     # Start sentry sdk if not in debug mode
     if not debug:
         sentry_sdk.init(
-                dsn=os.getenv('SENTRY_DSN'),
-
-                # Set traces_sample_rate to 1.0 to capture 100%
-                # of transactions for performance monitoring.
-                # We recommend adjusting this value in production.
-                traces_sample_rate=1.0
+            dsn=os.getenv("SENTRY_DSN"),
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            # We recommend adjusting this value in production.
+            traces_sample_rate=1.0,
         )
 
     intents = discord.Intents.default()
@@ -139,23 +157,23 @@ async def main(debug: bool, db_version: int):
     bot = DatabaseBot(command_prefix="!", intents=intents, db_path=db_path, debug=debug)
 
     # Load extensions
-    await bot.load_extension('roller_bot.cogs.sync_commands')
-    await bot.load_extension('roller_bot.cogs.admin_commands')
+    await bot.load_extension("roller_bot.cogs.sync_commands")
+    await bot.load_extension("roller_bot.cogs.admin_commands")
 
-    await bot.load_extension('roller_bot.cogs.info_commands')
-    await bot.load_extension('roller_bot.cogs.user_commands')
-    await bot.load_extension('roller_bot.cogs.shop_commands')
-    await bot.load_extension('roller_bot.cogs.action_commands')
+    await bot.load_extension("roller_bot.cogs.info_commands")
+    await bot.load_extension("roller_bot.cogs.user_commands")
+    await bot.load_extension("roller_bot.cogs.shop_commands")
+    await bot.load_extension("roller_bot.cogs.action_commands")
 
     # Tasks
-    await bot.load_extension('roller_bot.cogs.random_event_task')
+    await bot.load_extension("roller_bot.cogs.random_event_task")
     # Load backup task if not in debug mode
-    #if not debug:
-        # await bot.load_extension('roller_bot.cogs.backup_task')
+    # if not debug:
+    # await bot.load_extension('roller_bot.cogs.backup_task')
 
     # Run the discord bot
-    token: str | None = os.getenv('DISCORD_TOKEN')
+    token: str | None = os.getenv("DISCORD_TOKEN")
     if token:
         await bot.run(token)
     else:
-        raise ValueError('No token was found in the environment variables')
+        raise ValueError("No token was found in the environment variables")
