408aff913b8e4a4dcce95a583852b996b8734806
Vegard Bjørsvik <vegardbjr@gmail.com>
2023-07-20 09:48:21 +0200

    nullable roll

--- roller_bot/models/roll.py
+++ roller_bot/models/roll.py
@@ -1,19 +1,21 @@
 from typing import List
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Boolean
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer
 from sqlalchemy.orm import relationship
+
 from roller_bot.models.base import Base
 from roller_bot.models.bonus_value import BonusValue
 
 
 class Roll(Base):
-    __tablename__: str = 'rolls'
+    __tablename__: str = "rolls"
     id: Column = Column(
-        Integer, primary_key=True,
-        autoincrement=True, nullable=False
-        )
-    user_id: Column = Column(Integer, ForeignKey('users.id'), nullable=False)
-    item_id: Column = Column(Integer, ForeignKey('items.id'), nullable=False)
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    user_id: Column = Column(Integer, ForeignKey("users.id"), nullable=False)
+    item_id: Column = Column(Integer, ForeignKey("items.id"), nullable=False)
     roll_time: Column = Column(DateTime, nullable=False)
     base_value: Column = Column(Integer, nullable=False)
     can_roll_again: Column = Column(Boolean, nullable=False, default=False)
@@ -23,7 +25,7 @@ class Roll(Base):
     bonus_values: List[BonusValue] = relationship("BonusValue", back_populates="roll")
 
     def __repr__(self) -> str:
-        return f'{self.base_value}'
+        return f"{self.base_value}"
 
     @property
     def total_value(self) -> int:
